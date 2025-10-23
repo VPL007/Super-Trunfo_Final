845e82c91538c2394b451fdc379eb2c6b9fd9394
VPL007 <71355250+VPL007@users.noreply.github.com>
2025-10-23 01:40:14 +0000

    Adicionado menu e as opções regras e cartas

--- SuperTrunfo.c
+++ SuperTrunfo.c
@@ -88,7 +88,105 @@ int main () {
                             (inverso_densidade_4 * 1000.0);     // amplificado o inverso da densidade
 
 
+//-------------------------Iniciar Jogo-------------------------------------------
+char opcao;
+char escolha_carta;
+char carta_jogador;
+char escolha_cidade;
+int sair = 0;
 
+while(!sair){    
+printf("\n==================================================\n");
+printf("  BEM VINDO AO SUPER TRUNFO!!! VAMOS COMEÇAR?\n");
+printf("====================================================\n");
+
+printf("Menu do jogo:\n");
+printf("I. Iniciar Jogo\n");
+printf("R. Regras\n");
+printf("C. Cartas cadastradas e seus dados\n");
+printf("S. Sair\n");
+
+printf("Escolha a sua opção:\n");
+scanf(" %c", &opcao);
+
+
+//-----------------------Regras--------------------------------------------------
+switch (opcao) //Pronto
+{
+    case 'R': 
+    case 'r':
+        printf("\n");
+        printf("O jogo Super Trunfo é muito simples:\n");
+        printf("1º Escolha a sua carta.\n");
+        printf("2º Escolha o atributo que deseja comparar.\n");
+        printf("3º O computador irá escolhar uma carta aleatória para jogar.\n");
+        printf("4º Vai aparecer na tela o resultado do jogo (quem venceu ou se deu empate)\n\n");
+        printf("OBS: Certifique-se de escolher uma carta válida,\n");
+        printf("     em caso de dúvidas, consulte a seção Cartas Cadastradas.");
+        printf("\n");
+    break;
+}
+//-----------------------Cartas Cadastradas-----------------
+switch (opcao) //Pronto
+{
+    case 'C':
+    case 'c':  
+    //Início da exibição dos dados das cartas
+        printf("Cartas cadastradas:\n");
+        printf("\n");
+        
+                printf("Carta 1:\n");
+                printf("Estado: %s\n", nome_estado_1);
+                printf("Código: %s\n", codigo_carta_1);
+                printf("Cidade: %s\n", nome_cidade_1);
+                printf("População: %d\n", populacao_1);
+                printf("Área: %.2f Km²\n", area_1);
+                printf("PIB: R$ %.2f Bilhões de Reais\n", pib_1);
+                printf("Pontos Turísticos: %d\n", pontos_turisticos_1);
+                printf("Densidade Populacional: %.2f hab/km²\n", densidade_pop_1);
+                printf("PIB per capita: R$ %.2f\n", pib_per_capita_1);
+                printf("Super Poder: %.2f\n", super_poder_1);         
+                printf("\n");
+
+                printf("Carta 2:\n");
+                printf("Estado: %s\n", nome_estado_2);
+                printf("Código: %s\n", codigo_carta_2);
+                printf("Cidade: %s\n", nome_cidade_2);
+                printf("População: %d\n", populacao_2);
+                printf("Área: %.2f Km²\n", area_2);
+                printf("PIB: R$ %.2f Bilhões de Reais\n", pib_2);
+                printf("Pontos Turísticos: %d\n", pontos_turisticos_2);
+                printf("Densidade Populacional: %.2f hab/km²\n", densidade_pop_2);
+                printf("PIB per capita: R$ %.2f\n", pib_per_capita_2);
+                printf("Super Poder: %.2f\n", super_poder_2);         
+                printf("\n");
+
+                printf("Carta 3:\n");
+                printf("Estado: %s\n", nome_estado_3);
+                printf("Código: %s\n", codigo_carta_3);
+                printf("Cidade: %s\n", nome_cidade_3);
+                printf("População: %d\n", populacao_3);
+                printf("Área: %.2f Km²\n", area_3);
+                printf("PIB: R$ %.2f Bilhões de Reais\n", pib_3);
+                printf("Pontos Turísticos: %d\n", pontos_turisticos_3);
+                printf("Densidade Populacional: %.2f hab/km²\n", densidade_pop_3);
+                printf("PIB per capita: R$ %.2f\n", pib_per_capita_3);
+                printf("Super Poder: %.2f\n", super_poder_3);         
+                printf("\n");
+
+                printf("Carta 4:\n");
+                printf("Estado: %s\n", nome_estado_4);
+                printf("Código: %s\n", codigo_carta_4);
+                printf("Cidade: %s\n", nome_cidade_4);
+                printf("População: %d\n", populacao_4);
+                printf("Área: %.2f Km²\n", area_4);
+                printf("PIB: R$ %.2f Bilhões de Reais\n", pib_4);
+                printf("Pontos Turísticos: %d\n", pontos_turisticos_4);
+                printf("Densidade Populacional: %.2f hab/km²\n", densidade_pop_4);
+                printf("PIB per capita: R$ %.2f\n", pib_per_capita_4);
+                printf("Super Poder: %.2f\n", super_poder_4);         
+                printf("\n");
+}
 
 
 
